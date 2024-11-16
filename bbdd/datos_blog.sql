2b459b96ee79d83d7583ff7d305eb1ca6094c0a5
memecito <memecito@gmail.com>
2024-11-16 22:06:20 +0100

    base de datos

---
+++ bbdd/datos_blog.sql
@@ -0,0 +1,49 @@
+INSERT INTO `autores` (`nombre`, `email`, `imagen` )VALUES 
+('Juan Pérez','juanperez@example.com','juan.jpg'),
+('Ana Gómez','anagomez@example.com','ana.jpg'),
+('Carlos López','carloslopez@example.com','carlos.jpg'),
+('María Fernández','mariafernandez@example.com','maria.jpg'),
+('Luis Martinez','luismartinez@example.com','martinez.jpg'),
+('Laura García','lauragarcia@example.com','laura.jpg'),
+('Diego Torres','diegotorres@example.com','diego.jpg'),
+('Sara Castillo','saracastillo@example.com','sara.jpg'),
+('Jorge Martínez','jorgemartinez@example.com','jorge.jpg'),
+('Paula Méndez','paulamendez@example.com','paula.jpg'),
+('Raúl Romero','raulromero@example.com','raul.jpg'),
+('Daniela Rivas','danielarivas@example.com','daniela.jpg'),
+('Mario Vargas','mariovargas@example.com','mario.jpg'),
+('Eva Sánchez','evasanchez@example.com','eva.jpg'),
+('Gabriel Herrera','gabrielherrera@example.com','gabriel.jpg'),
+('Patricia Silva','patriciasilva@example.com','patricia.jpg'),
+('Andrés Navarro','andresnavarro@example.com','andres.jpg'),
+('Mónica Castro','monicastro@example.com','monica.jpg'),
+('Elena Reyes','elenareyes@example.com','elena.jpg'),
+('Tomás Luna','tomasluna@example.com','tomas.jpg'),
+('antonio','antonio@antonio.com','antonio.jpg'),
+('luis','luis@antonio.com','luis.jpg'),
+('Maria','Maria@antonio.com','Maria.jpg'),
+('Aridna','Ariadna@antonio.com','Ariadna.jpg'),
+('Maria','Maria@antonio.com','Laura.jpg'),
+('AAA','AAA@antonio.com','Laura.jpg'),
+('BBB','BBB@antonio.com','Laura.jpg');
+
+
+ 
+ INSERT INTO `posts` (`titulo`,`descripcion`,`fecha_creacion`,`categoria`, `autor_id`) VALUES 
+ ('Introducción a la Inteligencia Artificial','Conceptos básicos de IA','2023-01-10','Tecnología',1),
+ ('Mejorando tu Salud Mental','Consejos prácticos','2023-02-15','Salud',2),
+ ('Métodos de enseñanza innovadores','Tendencias en educación','2023-03-01','Educación',3),
+ ('Lugares imprescindibles de Europa','Guía de viajes','2023-04-20','Viajes',4),
+ ('Ejercicios para mantenerte en forma','Rutina de ejercicios','2023-05-05','Deportes',5),
+ ('Emprendimiento en la era digital','Consejos para emprendedores','2023-06-12','Negocios',6),
+ ('Evolución de los videojuegos','Historia del gaming','2023-08-18','Gaming',10),
+ ('la metafisica del tiempo','estudio sobre el tiempo y sus derivados','2024-10-23','fisica',7),
+ ('la metafisica del tiempo','estudio sobre el tiempo y sus derivados','2024-10-23','fisica',8),
+ ('El estudio de lo irracional','Tesis doctoral sobre lo irracional y como el mundo se va a la mierda','2024-10-23','fisica',8),
+ ('Como tener hijos y no morir en el intento','Libro muy educativo sobre como castigar a tus hijos por todo lo que haga','2024-10-23','fisica',9),
+ ('Te voy a dar como a cajon que no cierra','Manual sobre como mantener el control cuando tus hijos han roto la tele nueva de 5000€','2024-10-23','fisica',9),
+ ('Limpieza de codigo','Despues de horas programando te recomiendo una buena botella de vino y todo estara mas claro','2024-10-23','fisica',11),
+ ('Los futuros dictadores locos','Acompañame en este trepidante viaje para conocer de primera mano quien mandara el mundo a la mierda','2024-10-23','Distopia',12),
+ (28,'el arte de programar y no volverte loco','Manual de programacion y como estrellar el ordenador cuando no sabes donde esta el fallo','2024-10-23','Informática',13);
+
+
